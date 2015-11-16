073122e17e8f3216da4fe4579ce857db71c845f5
cibernox <miguel.camba@gmail.com>
2015-11-16 11:56:16 +0000

    Basic typeahead

--- addon/components/power-select-typeahead/selected.js
+++ addon/components/power-select-typeahead/selected.js
@@ -2,5 +2,39 @@ import Ember from 'ember';
 import layout from '../../templates/components/power-select-typeahead/selected';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  tagName: '',
+
+  // Actions
+  actions: {
+    captureClick(e) {
+      e.stopPropagation();
+    },
+
+    search(e) {
+      let term = e.target.value;
+      this.get('search')(term, e);
+      e.stopPropagation();
+      if (term.length > 0) {
+        this.get('dropdown').open(e);
+      } else {
+        this.get('dropdown').close(e, true);
+      }
+    },
+
+    handleKeydown(e) {
+      let dropdown = this.get('dropdown');
+      if (!dropdown.isOpen) {
+        e.stopPropagation();
+        return;
+      }
+      let term = e.target.value;
+      if (e.keyCode === 9) {
+        this.get('select')(this.get('highlighted'), dropdown, e);
+      }
+      if (term.length === 0) {
+        e.stopPropagation();
+      }
+    }
+  }
 });
