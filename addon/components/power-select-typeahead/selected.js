4dc710ca8f92013a7011e2446cc44cc4f0abfb0d
cibernox <miguel.camba@gmail.com>
2015-12-04 16:10:37 +0000

    Make typeahead work with EPS 0.6.4+

--- addon/components/power-select-typeahead/selected.js
+++ addon/components/power-select-typeahead/selected.js
@@ -7,26 +7,14 @@ export default Ember.Component.extend({
   layout: layout,
   tagName: '',
 
-  // Lifecycle hooks
-  didReceiveAttrs({ oldAttrs, newAttrs }) {
-    this._super(...arguments);
-    if (oldAttrs === undefined) { return; } // First time nothing to do
-    let { isOpen, actions: { open, close } } = newAttrs.select;
-
-    if (isOpen && !isBlank(oldAttrs.searchText) && isBlank(newAttrs.searchText)) {
-      return run.schedule('actions', close);
+  // Observers
+  optionsObserver: Ember.observer('options.length', function() {
+    if (this.get('options.length') > 0 && this.get('select.isOpen') === false) {
+      run.scheduleOnce('actions', this.get('select.actions.open'));
+    } else if (this.get('searchText.length') === 0 && this.get('select.isOpen') === true) {
+      run.scheduleOnce('actions', this.get('select.actions.close'));
     }
-
-    if (!isOpen) {
-      // is closed but a promise was just resolved and the search text is not empty
-      if (oldAttrs.hasPendingPromises && !newAttrs.hasPendingPromises && newAttrs.options && !isBlank(newAttrs.searchText)) {
-        return run.schedule('actions', open);
-      }
-    } else if (oldAttrs.options && get(oldAttrs.options, 'length') === 0 && !oldAttrs.hasPendingPromises && newAttrs.hasPendingPromises) {
-      // Options were empty (the "No matches" message was shown) and a new search starts, so the box must close
-      run.schedule('actions', close);
-    }
-  },
+  }),
 
   // Actions
   actions: {
