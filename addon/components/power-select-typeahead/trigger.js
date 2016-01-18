e1cdb1a5ba64390149ec00d7634d6fa0ba306580
cibernox <miguel.camba@gmail.com>
2016-01-18 15:57:28 +0000

    Released v0.3.1

--- addon/components/power-select-typeahead/trigger.js
+++ addon/components/power-select-typeahead/trigger.js
@@ -1,16 +1,33 @@
 import Ember from 'ember';
 import layout from '../../templates/components/power-select-typeahead/trigger';
 
+const { get } = Ember;
+
 export default Ember.Component.extend({
   layout: layout,
   tagName: '',
 
   // Observers
-  optionsObserver: Ember.observer('options.length', function() {
-    if (this.get('options.length') > 0 && this.get('select.isOpen') === false) {
-      this.get('select.actions.open')();
-    } else if (this.get('searchText.length') === 0 && this.get('select.isOpen') === true) {
-      this.get('select.actions.close')();
+  optionsObserver: Ember.observer('options.length', 'searchText.length', function() {
+    let select = this.get('select');
+    if (!this.get('searchText.length')) {
+      if (select.isOpen) { select.actions.close(); }
+      return;
+    }
+    let options = this.get('options');
+    if (get(options, 'isPending')) {
+      if (get(options, 'length') === 0) { select.actions.close(); }
+      this._lastSearch = options;
+      options.then((results) => {
+        if (this._lastSearch === options && !this.get('select.isOpen')) {
+          select.actions.open();
+        }
+      });
+      return;
+    }
+
+    if (select.isOpen === false) {
+      return select.actions.open();
     }
   }),
 
