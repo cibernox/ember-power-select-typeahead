8da2f84f93086b8739535bc01f8d5fde035fb389
cibernox <miguel.camba@gmail.com>
2016-02-16 17:52:14 +0000

    Adapt to power-select > 0.9.0
    Having a `lastSearchedText` makes the component MUCH simpler to implement :)

--- addon/components/power-select-typeahead/trigger.js
+++ addon/components/power-select-typeahead/trigger.js
@@ -1,35 +1,18 @@
 import Ember from 'ember';
 import layout from '../../templates/components/power-select-typeahead/trigger';
 
-const { get } = Ember;
+const { isBlank } = Ember;
 
 export default Ember.Component.extend({
   layout: layout,
   tagName: '',
 
-  // Observers
-  optionsObserver: Ember.observer('options.length', 'searchText.length', function() {
-    let select = this.get('select');
-    if (!this.get('searchText.length')) {
-      if (select.isOpen) { select.actions.close(); }
-      return;
+  didUpdateAttrs({ oldAttrs, newAttrs }) {
+    this._super(...arguments);
+    if (newAttrs.lastSearchedText !== oldAttrs.lastSearchedText) {
+      newAttrs.select.actions[isBlank(newAttrs.lastSearchedText) ? 'close' : 'open']();
     }
-    let options = this.get('options');
-    if (get(options, 'isPending')) {
-      if (get(options, 'length') === 0) { select.actions.close(); }
-      this._lastSearch = options;
-      options.then(() => {
-        if (this._lastSearch === options && !this.get('select.isOpen')) {
-          select.actions.open();
-        }
-      });
-      return;
-    }
-
-    if (select.isOpen === false) {
-      return select.actions.open();
-    }
-  }),
+  },
 
   // Actions
   actions: {
@@ -44,14 +27,6 @@ export default Ember.Component.extend({
     handleKeydown(e) {
       let select = this.get('select');
       if (!select.isOpen) {
-        e.stopPropagation();
-        return;
-      }
-      let term = e.target.value;
-      if (e.keyCode === 9 || e.keyCode === 13) {
-        select.actions.choose(this.get('highlighted'), e);
-      }
-      if (term.length === 0) {
         e.stopPropagation();
       }
     }
