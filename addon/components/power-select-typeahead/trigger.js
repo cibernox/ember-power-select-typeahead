2033602704451204cfcf645d4715b6d6e295fc72
cibernox <miguel.camba@gmail.com>
2016-04-07 19:50:04 +0100

    Improve component

--- addon/components/power-select-typeahead/trigger.js
+++ addon/components/power-select-typeahead/trigger.js
@@ -1,32 +1,36 @@
 import Ember from 'ember';
 import layout from '../../templates/components/power-select-typeahead/trigger';
 
-const { isBlank } = Ember;
+const { isBlank, run, get } = Ember;
 
 export default Ember.Component.extend({
   layout: layout,
   tagName: '',
 
+  // Lifecycle hooks
   didUpdateAttrs({ oldAttrs, newAttrs }) {
     this._super(...arguments);
     if (newAttrs.lastSearchedText !== oldAttrs.lastSearchedText) {
-      newAttrs.select.actions[isBlank(newAttrs.lastSearchedText) ? 'close' : 'open']();
+      if (isBlank(newAttrs.lastSearchedText)) {
+        run.schedule('actions', null, newAttrs.select.actions.close, null, true);
+      } else {
+        run.schedule('actions', null, newAttrs.select.actions.open)
+      }
+    } else if (!isBlank(newAttrs.lastSearchedText) && get(this, 'options.length') === 0 && this.get('loading')) {
+      run.schedule('actions', null, newAttrs.select.actions.close, null, true);
     }
   },
 
   // Actions
   actions: {
-    captureMouseDown(e) {
+    stopPropagation(e) {
       e.stopPropagation();
     },
 
-    search(term, e) {
-      this.get('select.actions.search')(term, e);
-    },
-
     handleKeydown(e) {
-      let select = this.get('select');
-      if (!select.isOpen) {
+      let isLetter = e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32; // Keys 0-9, a-z or SPACE
+      let isSpecialKeyWhileClosed = !isLetter && !this.get('select.isOpen') && [13, 27, 38, 40].indexOf(e.keyCode) > -1;
+      if (isLetter || isSpecialKeyWhileClosed) {
         e.stopPropagation();
       }
     }
