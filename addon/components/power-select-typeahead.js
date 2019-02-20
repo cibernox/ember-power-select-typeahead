f5689bc9eafeaf6fb9000944f5f4b4b3b51ae1fa
bellg3 <geoffrey.bell@rsa.com>
2019-02-19 21:17:55 -0500

    Make sure onKeyDown returns false if action returns false

--- addon/components/power-select-typeahead.js
+++ addon/components/power-select-typeahead.js
@@ -36,7 +36,9 @@ export default Component.extend({
       let action = this.get('onkeydown');
 
       // if user passes `onkeydown` action
-      if (!action || action(select, e) !== false) {
+      if (action || action(select, e) === false) {
+        return false;
+      } else {
         // if escape, then clear out selection
         if (e.keyCode === 27) {
           select.actions.choose(null);
